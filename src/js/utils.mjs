1b0d5ab8cded1311be322a858a6b854679c8edab
Rich-Wilkyness <r.christian.wilkerson@gmail.com>
2023-09-23 19:58:14 -0600

    completed individual2 with filter code

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -32,3 +32,11 @@ export function getParams(param) {
 
   return product;
 }
+
+export function renderListWithTemplate(templateFn, parentElement, list, position="afterbegin", clear=false) {
+  const htmlStrings = list.map(templateFn);
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
