9980cecafd5e952b42fbe426c41fb67bc681ddd4
Miles Noble <MilosDragons@gmail.com>
2023-10-05 11:43:37 -0600

    Personal Update/Code Condensing

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,45 +1,56 @@
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+
+function productDetailsTemplate(product) {
+  return `<section class="product-detail"> <h3>${product.Brand.Name}</h3>
+    <h2 class="divider">${product.NameWithoutBrand}</h2>
+    <img
+      class="divider"
+      src="${product.Images.PrimaryLarge}"
+      alt="${product.NameWithoutBrand}"
+    />
+    <p class="product-card__price">$${product.FinalPrice}</p>
+    <p class="product__color">${product.Colors[0].ColorName}</p>
+    <p class="product__description">
+    ${product.DescriptionHtmlSimple}
+    </p>
+    <div class="product-detail__add">
+      <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+    </div></section>`;
+}
 
 export default class ProductDetails {
-    constructor(productId, dataSource) {
-      this.productId = productId;
-      this.product = {};
-      this.dataSource = dataSource;
-    }
-    async init() {
-      // use our data source to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-      // once we have the product details we can render out the HTML
-      // once the HTML is rendered we can add a listener to Add to Cart button
-      // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-      this.product = await this.dataSource.findProductById(this.productId);
-      this.renderProductDetails("main");
-      document.getElementById("addToCart").addEventListener("click", this.addToCart.bind(this));
-    }
-    addToCart() {
-      let cartContents = getLocalStorage("so-cart");
-      //check to see if there was anything there
-      if (!cartContents) {
-        cartContents = [];
-      }
-      // then add the current product to the list
-      cartContents.push(this.product);
-      setLocalStorage("so-cart", cartContents);
-    }
-    renderProductDetails(selector) {
-      document.querySelector(selector).insertAdjacentHTML("afterBegin", productTemplate(this.product)
-      );
-    }
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
   }
-
-function productTemplate(data) {
-    return `<section class="product-detail">
-    <h3>${data.Brand.Name}</h3>
-    <h2 class="divider">${data.NameWithoutBrand}</h2>
-    <img class="divider" src="${data.Image}" alt="${data.NameWithoutBrand}"/>
-    <p class="product-card__price">${data.FinalPrice}</p>
-    <p class="product__color">${data.Colors[0].ColorName}</p>
-    <p class="product__description"> ${data.DescriptionHtmlSimple} </p>
-    <div class="product-detail__add">
-      <button id="addToCart" data-id="${data.Id}">Add to Cart</button>
-    </div>
-  </section>`
+  async init() {
+    // use our dataSource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+    this.product = await this.dataSource.findProductById(this.productId);
+    // once we have the product details we can render out the HTML
+    this.renderProductDetails("main");
+    // once the HTML is rendered we can add a listener to Add to Cart button
+    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
+    document.getElementById("addToCart").addEventListener("click", () => {
+      console.log("Clicked");
+      this.addToCart.bind(this);
+    });
+  }
+  addToCart() {
+    let cartContents = getLocalStorage("so-cart");
+    //check to see if there was anything there
+    if (!cartContents) {
+      cartContents = [];
+    }
+    // then add the current product to the list
+    cartContents.push(this.product);
+    setLocalStorage("so-cart", cartContents);
+  }
+  renderProductDetails(selector) {
+    const element = document.querySelector(selector);
+    element.insertAdjacentHTML(
+      "afterBegin",
+      productDetailsTemplate(this.product)
+    );
+  }
 }
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -9,13 +9,59 @@ export function qs(selector, parent = document) {
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
-
 // save data to local storage
 export function setLocalStorage(key, data) {
-  updateCartCount();
-  let dataArray = getLocalStorage(key) || [];
-  dataArray.push(data);
-  localStorage.setItem(key, JSON.stringify(dataArray));
+  localStorage.setItem(key, JSON.stringify(data));
+}
+
+// helper to get parameter strings
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
+// function to take a list of objects and a template and insert the objects as HTML into the DOM
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
+
+// function to take an optional object and a template and insert the objects as HTML into the DOM
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.insertAdjacentHTML("afterbegin", template);
+  //if there is a callback...call it and pass data
+  if (callback) {
+    callback(data);
+  }
+}
+
+async function loadTemplate(path) {
+  const res = await fetch(path);
+  const template = await res.text();
+  return template;
+}
+
+// function to dynamically load the header and footer into a page
+export async function loadHeaderFooter() {
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const headerElement = document.querySelector("#main-header");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
 }
 
 // set a listener for both touchend and click
@@ -25,51 +71,4 @@ export function setClick(selector, callback) {
     callback();
   });
   qs(selector).addEventListener("click", callback);
-}
-
-
-export function getParams(param) {
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get(param)
-
-  return product;
-}
-
-export function renderListWithTemplate(templateFn, parentElement, list, position="afterbegin", clear=false) {
-  const htmlStrings = list.map(templateFn);
-  if (clear) {
-    parentElement.innerHTML = "";
-  }
-  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
-}
-
-export function renderWithTemplate(template, parentElement, data, callback) {
-  parentElement.insertAdjacentHTML("afterbegin", template);
-  if(callback) {
-    callback(data);
-  }
-}
-
-export async function loadTemplate(path) {
-  const template = await fetch(path).then(res => res.text());
-  return template;
-}
-
-export async function loadHeaderFooter() {
-  const headerTemplate = await loadTemplate("../public/partials/header.html");
-  const footerTemplate = await loadTemplate("../public/partials/footer.html");
-  const headerElement = document.getElementById("main-header");
-  const footerElement = document.getElementById("main-footer");
-
-  renderWithTemplate(headerTemplate, headerElement)
-  renderWithTemplate(footerTemplate, footerElement)
-}
-
-export function updateCartCount() {
-  var cartCountElement = document.getElementById('cartCount');
-  var cartItemCount = parseInt(cartCountElement.textContent) + 1
-  if (cartCountElement) {
-    cartCountElement.textContent = cartItemCount.toString();
-  }
 }
