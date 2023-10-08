0343bf140835c5a04966486a711e6800ab8ed149
CamilaApai <camilaapai@hotmail.com>
2023-10-07 22:44:51 -0300

    Fix

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,10 +7,21 @@ const cart = new ShoppingCart("so-cart", ".product-list");
 cart.renderCartContents();
 
 const cartItems = cart.getCartItems();
-const total = cartItems.reduce((acc, item) => acc + item.FinalPrice, 0);
+const totalElement = document.querySelector(".list-total");    
+let total = 0;
 
-const totalElement = document.querySelector(".list-total");
-totalElement.innerHTML = `Total: $${total.toFixed(2)}`;
+// Total$ in Cart
+if (cartItems.length > 0) {
+
+    for (const item of cartItems) {
+      total += item.FinalPrice;
+    }
+
+    totalElement.innerHTML = `Total: $${total.toFixed(2)}`;
+
+} else {
+    totalElement.style.display = "none";
+  }
 
 const cartFooter = document.querySelector(".list-footer");
 cartFooter.classList.remove("hide");
