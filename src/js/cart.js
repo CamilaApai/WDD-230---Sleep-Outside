1f201a1feb9edc35bb61c5aee26435d883af7200
Rich-Wilkyness <r.christian.wilkerson@gmail.com>
2023-12-21 06:35:19 -0700

    trying to get live website working

--- src/js/cart.js
+++ src/js/cart.js
@@ -11,18 +11,18 @@ if (cart.total > 0) {
 }
 
 const cartItems = cart.getCartItems();
-const totalElement = document.querySelector(".list-total");    
+const totalElement = document.querySelector(".list-total");
 let total = 0;
 
 // Total$ in Cart
 if (cartItems.length > 0) {
-    for (const item of cartItems) {
-      total += item.FinalPrice;
-    }
-    totalElement.innerHTML = `Total: $${total.toFixed(2)}`;
-} else {
-    totalElement.style.display = "none";
+  for (const item of cartItems) {
+    total += item.FinalPrice;
   }
+  totalElement.innerHTML = `Total: $${total.toFixed(2)}`;
+} else {
+  totalElement.style.display = "none";
+}
 
 const cartFooter = document.querySelector(".list-footer");
 cartFooter.classList.remove("hide");
